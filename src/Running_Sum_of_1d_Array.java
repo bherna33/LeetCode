a5bba6af03ea460236cbf910714f28d824f9e1e3
Branden Henrnadez <Brandenjh98@gmail.com>
2021-11-04 12:04:47 -0400

    Running Sum of 1d Array

---
+++ src/Running_Sum_of_1d_Array.java
@@ -0,0 +1,15 @@
+public class Running_Sum_of_1d_Array {
+    public int[] runningSum(int[] nums) {
+
+        int [] ans = new int[nums.length];      // create array
+
+        int sums = 0;       // create sums
+
+        for(int i = 0; i < nums.length; i++){   // loop through array
+            sums = sums + nums[i];      // current sums + nums_i
+            ans[i] = sums;      // add sums to array
+        }
+
+        return ans;     // return array
+    }
+}
