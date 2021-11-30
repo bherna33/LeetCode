2c2afa0777cf7126f1604dc13fce2ddccb4c8f77
Branden Henrnadez <Brandenjh98@gmail.com>
2021-11-30 14:19:38 -0500

    Added: Goal Parser Interpretation

---
+++ src/Goal_Parser_Interpretation/Solution.java
@@ -0,0 +1,9 @@
+package Goal_Parser_Interpretation;
+
+public class Solution {
+    public String interpret(String command) {
+        String s1 = command.replace("()", "o");
+        String s2 = s1.replace("(al)", "al");
+        return s2;
+    }
+}
