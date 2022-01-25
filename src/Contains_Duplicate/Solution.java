c37681c66dabc83aa275586cc21e322cef54c88a
Branden Henrnadez <Brandenjh98@gmail.com>
2022-01-25 14:57:22 -0500

    Added: Contains Duplicate

--- src/Contains_Duplicate/Solution.java
+++ src/Contains_Duplicate/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);      //sort the array
         for(int i =1; i< nums.length; i++){     //loop through array starting from the second array
-            if(nums[i] == nums[i-1]){       // comepars the ith and the i-1 element
+            if(nums[i] == nums[i-1]){       // compare the ith and the i-1 element
                 return true;        //returns true if same
             }
         }
