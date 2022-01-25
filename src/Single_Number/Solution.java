60450f59e1e67e334c3fef3480cc660191dd9a3d
Branden Henrnadez <Brandenjh98@gmail.com>
2022-01-25 13:26:08 -0500

    Added: Single Number

--- src/Single_Number/Solution.java
+++ src/Single_Number/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public int singleNumber(int[] nums) {
         int current = nums[0];      //this assumes that the first element is the single number
         for(int i = 1; i < nums.length; i++){
-            current = current ^ nums[i];        // compares to see if element is the same
+            current = current ^ nums[i];        // XOR of the same values
         }
         return current;     //returns single number
     }
